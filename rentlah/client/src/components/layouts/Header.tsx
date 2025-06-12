bebe2511fa306cfbba6e3cbc38e7f25106522a2d
wltdwinnie <wl.thawdar.2024@smu.edu.sg>
2025-06-13 02:28:42 +0800

    feat: added slide-in notification sidebar

--- rentlah/client/src/components/layouts/Header.tsx
+++ rentlah/client/src/components/layouts/Header.tsx
@@ -2,13 +2,18 @@
 
 import Image from 'next/image';
 import styles from './Header.module.css';
+import Link from 'next/link';
 import { UniversityDropdown } from '@/components/features/university-select';
 import { usePathname, useRouter } from 'next/navigation';
+import { useState } from "react";
+import { Bell } from "lucide-react";
+import Notification from "../features/Notification";
 
 export default function Header() {
   const pathname = usePathname();
   const router = useRouter();
   const isHomePage = pathname === '/';
+  const [isNotificationOpen, setIsNotificationOpen] = useState(false);
 
   const handleUniversitySelect = (uni: string) => {
     // Extract short name if it's in the full format
@@ -18,6 +23,7 @@ export default function Header() {
   };
 
   return (
+    <>
     <header className={styles.header}>
       {/* Temporary logo */}
       <div className={styles.logoContainer}>
@@ -32,12 +38,24 @@ export default function Header() {
         />
       )}
 
+
       {/* Action Buttons */}
-      <div className={styles.actions}>
-        <button className={styles.bell}>⩍</button>
-        <button className={styles.login}>Login</button>
-        <button className={styles.signup}>Sign Up</button>
-      </div>
-    </header>
+        <div className={styles.actions}>
+          <button 
+            className={styles.bell}
+            onClick={() => setIsNotificationOpen(!isNotificationOpen)}
+          >
+            <Bell className="h-6 w-6" />
+          </button>
+
+          <button className={styles.login}>Login</button>
+          <button className={styles.signup}>Sign Up</button>
+        </div>
+      </header>
+      <Notification
+        isOpen={isNotificationOpen}
+        onClose={() => setIsNotificationOpen(false)}
+      />
+    </>
   );
 }
