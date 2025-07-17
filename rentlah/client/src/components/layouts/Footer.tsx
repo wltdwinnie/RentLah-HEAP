c24d573a6c43b43503fc0faa9ae41c6ef3d44676
Htet Shwe <htetshwewinthan@gmail.com>
2025-07-17 22:15:13 +0800

    feat: adding property

--- rentlah/client/src/components/layouts/Footer.tsx
+++ rentlah/client/src/components/layouts/Footer.tsx
@@ -1,37 +1,46 @@
-import styles from './Footer.module.css';
+import styles from "./Footer.module.css";
 import { Mail, Instagram, Facebook } from "lucide-react";
+import AddPropertyButton from "./AddPropertyButton";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.contacts}>
-        <a
-          href="mailto:rentlahsg@gmail.com"
-          className={styles.contactItem}
-          title="Email"
-        >
-          <Mail className="h-6 w-6" />
-        </a>
-        <a
-          href="https://instagram.com/rentlahsg"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.contactItem}
-          title="Instagram"
-        >
-          <Instagram className="h-6 w-6" />
-        </a>
-        <a
-          href="https://facebook.com/rentlahsg"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.contactItem}
-          title="Facebook"
-        >
-          <Facebook className="h-6 w-6" />
-        </a>
+      <div className={styles.footerRow}>
+        <div className={styles.contacts}>
+          <a
+            href="mailto:rentlahsg@gmail.com"
+            className={styles.contactItem}
+            title="Email"
+          >
+            <Mail className="h-6 w-6" />
+          </a>
+          <a
+            href="https://instagram.com/rentlahsg"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.contactItem}
+            title="Instagram"
+          >
+            <Instagram className="h-6 w-6" />
+          </a>
+          <a
+            href="https://facebook.com/rentlahsg"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.contactItem}
+            title="Facebook"
+          >
+            <Facebook className="h-6 w-6" />
+          </a>
+        </div>
+        <div className={styles.ctaColumn}>
+          <div className={styles.ctaText}>
+            List your housing here for students
+          </div>
+          <AddPropertyButton />
+        </div>
       </div>
       <br></br>
       <div className={styles.text}>
