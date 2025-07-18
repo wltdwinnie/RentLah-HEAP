edbe6d5e2396a6433c2b7a16ae98fbebecddcf3a
Htet Shwe <htetshwewinthan@gmail.com>
2025-07-18 22:09:03 +0800

    style: changes to property card and authentication messages

--- rentlah/client/src/components/propertycard-group.tsx
+++ rentlah/client/src/components/propertycard-group.tsx
@@ -7,7 +7,7 @@ interface PropertyCardGroupProps {
 
 const PropertyCardGroup = ({ listings } : PropertyCardGroupProps) => {
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {listings.map((listing) => (
             <PropertyCard key={listing.id} listing={listing} />
         ))}
--- rentlah/client/src/lib/auth.ts
+++ rentlah/client/src/lib/auth.ts
@@ -5,41 +5,61 @@ import dotenv from "dotenv";
 dotenv.config();
 // Database pool for custom queries
 export const dbPool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 });
 export const auth = betterAuth({
-    secretKey: process.env.BETTER_AUTH_SECRET,
-    database: dbPool,
-    emailAndPassword: {
-        enabled: true,
-        autoSignIn: true,
-        requireEmailVerification: true,
-        sendResetPassword: async ({ user, url }) => {
-            await sendEmail({
-                to: user.email,
-                subject: "Reset your password",
-                text: `Click the link to reset your password: ${url}`,
-            });
-        }
+  secretKey: process.env.BETTER_AUTH_SECRET,
+  database: dbPool,
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+    requireEmailVerification: true,
+    sendResetPassword: async ({ user, url }) => {
+      await sendEmail({
+        to: user.email,
+        subject: "Reset your password",
+        text: `
+                Dear ${user.name},
+                We received a request to reset your password. You can reset it by clicking the link below:
+                
+                🔐 ${url}
+                
+                If you did not request a password reset, please ignore this message. This link will expire in 24 hours for your security.
+
+                Thank you,
+                RentLah Team
+                `,
+      });
     },
-        emailVerification: {
-            sendVerificationEmail: async ({ user, url }) => {
-                await sendEmail({
-                    to: user.email,
-                    subject: "Verify your email address",
-                    text: `Click the link to verify your email: ${url}`,
-                });
-            },
-        },
-        socialProviders: {
-            google: {
-                clientId: process.env.GOOGLE_CLIENT_ID || "",
-                clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-                redirectUri: process.env.GOOGLE_REDIRECT_URL || ""
-            },
-        },
-        session: {
-            expiresIn: 60 * 60 * 24 * 14, // 14 day (in seconds)
-            updateAge: 60 * 60 * 24 // Refresh session tokens daily
-        }
-    });
+  },
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url }) => {
+      await sendEmail({
+        to: user.email,
+        subject: "Verify your email address",
+        text: `
+                Dear ${user.name},
+                We received a request to create an account with this email. You can verify it by clicking the link below:
+
+                🔐 ${url}
+
+                If you did not request this, please ignore this message. This link will expire in 24 hours for your security.
+
+                Thank you,
+                RentLah Team
+                `,
+      });
+    },
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      redirectUri: process.env.GOOGLE_REDIRECT_URL || "",
+    },
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 14, // 14 day (in seconds)
+    updateAge: 60 * 60 * 24, // Refresh session tokens daily
+  },
+});
