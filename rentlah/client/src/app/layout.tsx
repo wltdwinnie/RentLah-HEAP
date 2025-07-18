de91af9189c8fc340c85b48b9d3cccdf6b26d020
Htet Shwe <htetshwewinthan@gmail.com>
2025-07-18 15:49:22 +0800

    style: make the default theme light

--- rentlah/client/src/app/layout.tsx
+++ rentlah/client/src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <body>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
