d2e9c76ccc795416764d0ab5d70196a90f11fac6
wltdwinnie <wl.thawdar.2024@smu.edu.sg>
2025-07-18 22:03:13 +0800

    Fix

--- rentlah/client/src/lib/socketClient.ts
+++ rentlah/client/src/lib/socketClient.ts
@@ -3,6 +3,7 @@ import { io, Socket } from "socket.io-client";
 
 let socketInstance: Socket | null = null;
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
 const SOCKET_SERVER_URL =
   typeof window !== "undefined" && process.env.NEXT_PUBLIC_SOCKET_URL
     ? process.env.NEXT_PUBLIC_SOCKET_URL
